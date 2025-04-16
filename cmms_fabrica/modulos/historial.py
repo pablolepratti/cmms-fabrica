702b050d1abb649ca4a2637917d825fec5da9d4e
Pablo Lepratti <106892195+pablolepratti@users.noreply.github.com>
2025-04-16 15:54:45 -0300

    Update historial.py

--- cmms_fabrica/modulos/historial.py
+++ cmms_fabrica/modulos/historial.py
@@ -1,25 +1,20 @@
-import csv
-import os
 from datetime import datetime
+from modulos.conexion_mongo import db
 
-RUTA_CSV = os.path.join("data", "historial.csv")
+# Referencia a la colección 'historial' en la base cmms_fabrica
+coleccion_historial = db["historial"]
 
-# Registrar evento en historial
 def log_evento(usuario, evento, id_referencia, tipo_origen, detalle):
     fila = {
-        "fecha": datetime.today().strftime("%Y-%m-%d %H:%M"),
+        "fecha": datetime.now(),
         "usuario": usuario,
         "evento": evento,
         "id_referencia": id_referencia,
         "tipo_origen": tipo_origen,
         "detalle": detalle
     }
-    archivo_existe = os.path.isfile(RUTA_CSV)
+
     try:
-        with open(RUTA_CSV, mode="a", newline='', encoding="utf-8") as file:
-            writer = csv.DictWriter(file, fieldnames=fila.keys())
-            if not archivo_existe:
-                writer.writeheader()
-            writer.writerow(fila)
+        coleccion_historial.insert_one(fila)
     except Exception as e:
-        print(f"[ERROR] No se pudo registrar en historial: {e}")
+        print(f"[ERROR] No se pudo registrar en historial (Mongo): {e}")
