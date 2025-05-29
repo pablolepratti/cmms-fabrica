1cc91d006cbf162c3d4c15658aad51fe21f8fef7
Pablo Lepratti <106892195+pablolepratti@users.noreply.github.com>
2025-05-29 17:31:07 -0300

    Update crud_activos_tecnicos.py

--- cmms_fabrica/crud/crud_activos_tecnicos.py
+++ cmms_fabrica/crud/crud_activos_tecnicos.py
@@ -1,10 +1,7 @@
 import streamlit as st
-from pymongo import MongoClient
 from datetime import datetime
+from modulos.conexion_mongo import db
 
-# Conexión MongoDB
-client = MongoClient(st.secrets["mongo_uri"])
-db = client["cmms"]
 coleccion = db["activos_tecnicos"]
 
 st.title("🔧 Gestión de Activos Técnicos")
