8af03c7b8e34cd7660522d91db533f89219b996c
Pablo Lepratti <106892195+pablolepratti@users.noreply.github.com>
2025-06-28 23:31:29 -0300

    Update crud_tareas_tecnicas.py

--- cmms_fabrica/crud/crud_tareas_tecnicas.py
+++ cmms_fabrica/crud/crud_tareas_tecnicas.py
@@ -11,8 +11,8 @@ Se registran automáticamente en la colección `historial` para trazabilidad.
 
 import streamlit as st
 from datetime import datetime
-from cmms_fabrica.modulos.conexion_mongo import db
-from cmms_fabrica.crud.generador_historial import registrar_evento_historial
+from modulos.conexion_mongo import db
+from crud.generador_historial import registrar_evento_historial
 
 coleccion = db["tareas_tecnicas"]
 
