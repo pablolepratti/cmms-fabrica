092c1c9621adc183c950424eb06e99e7c3b19f5b
Pablo Lepratti <106892195+pablolepratti@users.noreply.github.com>
2025-06-28 23:29:17 -0300

    Update crud_activos_tecnicos.py

--- cmms_fabrica/crud/crud_activos_tecnicos.py
+++ cmms_fabrica/crud/crud_activos_tecnicos.py
@@ -12,8 +12,8 @@ Registra automáticamente los eventos en la colección `historial` para trazabil
 
 import streamlit as st
 from datetime import datetime
-from cmms_fabrica.modulos.conexion_mongo import db
-from cmms_fabrica.crud.generador_historial import registrar_evento_historial
+from modulos.conexion_mongo import db
+from crud.generador_historial import registrar_evento_historial
 
 coleccion = db["activos_tecnicos"]
 
