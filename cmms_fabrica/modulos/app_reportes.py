842399b9c8a498e6a8416d13699c0e9b9055c9fa
Pablo Lepratti <106892195+pablolepratti@users.noreply.github.com>
2025-06-28 23:33:00 -0300

    Update app_reportes.py

--- cmms_fabrica/modulos/app_reportes.py
+++ cmms_fabrica/modulos/app_reportes.py
@@ -13,7 +13,7 @@ import streamlit as st
 import pandas as pd
 from fpdf import FPDF
 from datetime import datetime, date
-from cmms_fabrica.modulos.conexion_mongo import db
+from modulos.conexion_mongo import db
 import os
 from io import BytesIO
 
