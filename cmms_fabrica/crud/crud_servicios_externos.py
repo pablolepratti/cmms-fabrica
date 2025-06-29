031c793b01a656154d2b869a90d8ad53bbc7945a
Pablo Lepratti <106892195+pablolepratti@users.noreply.github.com>
2025-06-28 23:31:01 -0300

    Update crud_servicios_externos.py

--- cmms_fabrica/crud/crud_servicios_externos.py
+++ cmms_fabrica/crud/crud_servicios_externos.py
@@ -11,8 +11,8 @@ Cada cambio se documenta automáticamente en la colección `historial`.
 
 import streamlit as st
 from datetime import datetime
-from cmms_fabrica.modulos.conexion_mongo import db
-from cmms_fabrica.crud.generador_historial import registrar_evento_historial
+from modulos.conexion_mongo import db
+from crud.generador_historial import registrar_evento_historial
 
 coleccion = db["servicios_externos"]
 
